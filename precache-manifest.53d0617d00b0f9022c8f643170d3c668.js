bcc9d529216deeb0778983145e9b940357c360b7
monkeyray <raynier.v.dam@gmail.com>
2019-07-15 19:56:00 +0200

    Updates

---
+++ precache-manifest.53d0617d00b0f9022c8f643170d3c668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b815719d1c679edf2e66d7aa79213fc0",
+    "url": "/tidy/index.html"
+  },
+  {
+    "revision": "859748930fd5439a7057",
+    "url": "/tidy/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/tidy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "859748930fd5439a7057",
+    "url": "/tidy/static/js/main.914456f6.chunk.js"
+  },
+  {
+    "revision": "45ccedba99ca1f995596",
+    "url": "/tidy/static/js/runtime~main.691d123c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tidy/static/media/logo.5d5d9eef.svg"
+  }
+]);
