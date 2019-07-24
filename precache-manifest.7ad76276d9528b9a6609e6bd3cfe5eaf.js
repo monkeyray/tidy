736e4e49c448c60250eb84e014af249250346d8a
monkeyray <raynier.v.dam@gmail.com>
2019-07-24 15:04:22 +0200

    Updates

---
+++ precache-manifest.7ad76276d9528b9a6609e6bd3cfe5eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa86b0988a1a2727c2bfd6892a6d118",
+    "url": "/tidy/index.html"
+  },
+  {
+    "revision": "b9a7595420f247272d6e",
+    "url": "/tidy/static/css/main.75d7c6e6.chunk.css"
+  },
+  {
+    "revision": "4c1ff1335478a224b3ef",
+    "url": "/tidy/static/js/2.4358f433.chunk.js"
+  },
+  {
+    "revision": "b9a7595420f247272d6e",
+    "url": "/tidy/static/js/main.ed85c246.chunk.js"
+  },
+  {
+    "revision": "45ccedba99ca1f995596",
+    "url": "/tidy/static/js/runtime~main.691d123c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tidy/precache-manifest.5c37584a6fb8b08d08462e00a1f0fcdf.js"
+  "/tidy/precache-manifest.7ad76276d9528b9a6609e6bd3cfe5eaf.js"
 );
 
 self.addEventListener('message', (event) => {
